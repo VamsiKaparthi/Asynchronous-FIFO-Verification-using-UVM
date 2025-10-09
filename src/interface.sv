dc0e7c7399fbc675979e1eb95842d985f68668e1
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-10-09 10:14:33 +0530

    Update interface.sv

--- src/interface.sv
+++ src/interface.sv
@@ -1,1 +1,18 @@
+interface inf(input bit rclk, input bit rrst_n, input bit wclk, input bit wrst_n);
+  //inputs
+  logic rinc, winc;
+  logic [DSIZE - 1 : 0] wdata;
+  //outputs
+  logic wfull, rempty;
+  logic [DSIZE - 1 : 0] rdata;
 
+  clocking write_cb @(posedge wclk);
+    input winc, wrst_n, wdata;
+    input wfull;
+  endclocking
+
+  clocking read_cb @(posedge rclk);
+    input rinc, rrst_n;
+    input rempty, rdata;
+  endclocking
+endinterface
