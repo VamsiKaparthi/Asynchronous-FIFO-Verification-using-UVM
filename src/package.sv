ca911eb9b10fbadb021372e8c0e0123e3b6f84a1
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-10-11 13:13:30 +0530

    Create package.sv

---
+++ src/package.sv
@@ -0,0 +1,18 @@
+package pkg;
+        `include "uvm_macros.svh"
+        import uvm_pkg::*;
+        `include "define.svh"
+        `include "seq_item.sv"
+        `include "sequencer.sv"
+        `include "sequence.sv"
+  `include "wr_driver.sv"
+        `include "rd_driver.sv"
+        `include "wr_monitor.sv"
+        `include "rd_monitor.sv"
+        `include "write_agent.sv"
+        `include "read_agent.sv"
+        `include "scoreboard.sv"
+        `include "coverage.sv"
+        `include "environment.sv"
+        `include "test.sv"
+endpackage
