fbbe0294f55026ebb8ea3de74d0f1d7036eaa0c8
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-10-11 13:15:56 +0530

    Update scoreboard.sv

--- src/scoreboard.sv
+++ src/scoreboard.sv
@@ -13,7 +13,9 @@ class scoreboard extends uvm_scoreboard;
         int wptr, rptr;
         bit wfull = 0;
         bit rempty = 1;
-
+        bit prev_wfull;
+        bit prev_rempty = 1;
+        int prev_rdata;
         function new(string name = "scoreboard", uvm_component parent = null);
                 super.new(name, parent);
                 wr_item_collect_scb = new("wr_item_collect_scb", this);
@@ -30,61 +32,60 @@ class scoreboard extends uvm_scoreboard;
         endfunction
 
         int wr_pass, rd_pass;
+
         task writer();
+                $display("HIGH");
                 forever begin
                         wait(wr_q.size > 0);
                         wr_pkt = wr_q.pop_front();
-                        //`uvm_info("SCB", $sformatf("winc = %0b | wrst_n = %0b | wdata = %d || wfull = %0b", wr_pkt.winc, wr_pkt.wrst_n, wr_pkt.wdata, wr_pkt.wfull), UVM_MEDIUM);
-                        wfull = (mem.size == DEPTH);
+                        $display("-------------mem before write = %0p-------------", mem);
                         if(!wr_pkt.wrst_n)begin
                                 wfull = 0;
-                        //      wptr = 0;
                         end
-                        else if(wr_pkt.winc && !wfull)begin
-                                //$display("Enter");
-                                mem.push_back(wr_pkt.wdata);
-                        //      wptr = (wptr + 1) % DEPTH;
-                                wfull = (mem.size == DEPTH);
+                        else begin
+                                if(wr_pkt.winc && !prev_wfull)begin
+                                        mem.push_back(wr_pkt.wdata);
+                                end
                         end
-                        ref_pkt.wfull = wfull;
-                        `uvm_info("SCB WRITE REF OUT", $sformatf("wfull = %0b", ref_pkt.wfull), UVM_MEDIUM);
-                        $display("-------mem after write = %0p----------", mem);
-                        //Compare outputs
-                        if(ref_pkt.wfull === wr_pkt.wfull)begin
-                                `uvm_info("SCB WR", "WRITE PASS", UVM_MEDIUM);
+                        if(prev_wfull == wr_pkt.wfull)begin
+                                `uvm_info("SCB RD", "WRITE PASS", UVM_LOW);
                                 wr_pass++;
                         end
                         else begin
-                                `uvm_info("SCB WR", "WRITE FAIL", UVM_MEDIUM);
+                                `uvm_info("SCB RD", $sformatf("WRITE FAIL || scb wfull = %0d | mon wfull = %0d", prev_wfull, wr_pkt.wfull), UVM_LOW);
                         end
+                        $display("-------------mem after write = %0p-------------", mem);
+                        prev_wfull = (mem.size == 15);
                 end
+
         endtask
 
         task reader();
                 forever begin
                         wait(rd_q.size > 0);
                         rd_pkt = rd_q.pop_front();
-                        //`uvm_info("SCB", $sformatf("rinc = %0b | rrst_n = %0b || rdata = %d | rempty = %0b", rd_pkt.rinc, rd_pkt.rrst_n, rd_pkt.rdata, rd_pkt.rempty), UVM_MEDIUM);
-                        i//rempty = (mem.size == 0);
-                        $display("mem before read = %0p", mem);
+                        $display("-------------mem before read = %0p-------------", mem);
                         if(!rd_pkt.rrst_n)begin
                                 rempty = 1;
                         end
-                        else if(rd_pkt.rinc && !rempty)begin
-                                $display("Enter");
-                                ref_pkt.rdata = mem.pop_front();
-                                rptr = (rptr + 1) % DEPTH;
+                        else begin
+                                if(rd_pkt.rinc && !rempty)begin
+                                        ref_pkt.rdata = mem.pop_front();
+                                end
                         end
-                        ref_pkt.rempty = rempty;
-                        `uvm_info("SCB READ REF OUT", $sformatf("rdata = %d | rempty = %0b", ref_pkt.rdata, ref_pkt.rempty), UVM_MEDIUM);
-                        $display("-----mem after read = %0p----", mem);
-                        if(ref_pkt.rempty === rd_pkt.rempty && ref_pkt.rdata == rd_pkt.rdata)begin
-                                `uvm_info("SCB RD", "READ PASS", UVM_MEDIUM);
+                        if(rempty == rd_pkt.rempty && rempty)begin
+                                `uvm_info("SCB RD", "READ PASS", UVM_LOW);
+                                rd_pass++;
+                        end
+                        else if(ref_pkt.rdata == rd_pkt.rdata && rempty == rd_pkt.rempty)begin
+                                `uvm_info("SCB RD", "READ PASS", UVM_LOW);
                                 rd_pass++;
                         end
                         else begin
-                                `uvm_info("SCB RD", "READ FAIL", UVM_MEDIUM);
+                                `uvm_info("SCB RD", $sformatf("READ FAIL || scb rdata = %0d | mon rdata = %0d || scb rempty = %0d | mon rempty = %0d", ref_pkt.rdata, rd_pkt.rdata, rempty, rd_pkt.rempty), UVM_LOW);
                         end
+                        $display("-------------mem after read = %0p-------------", mem);
+                        rempty = (mem.size == 0);
                 end
         endtask
 
@@ -104,3 +105,35 @@ class scoreboard extends uvm_scoreboard;
                 `uvm_info("FINAL SCB RESULTS", $sformatf("write_passes = %0d || read_passes = %0d", wr_pass, rd_pass), UVM_MEDIUM);
         endfunction
 endclass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
