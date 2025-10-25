25845797b3d8aaefbe8204c3065dc15bfeb1e82f
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-10-25 20:26:40 +0530

    Update seq_item.sv

--- src/seq_item.sv
+++ src/seq_item.sv
@@ -1,28 +1,34 @@
-class seq_item  extends uvm_sequence_item;
-        //Input
-        rand bit winc;
-        rand bit rinc;
-        //rand bit wrst_n;
-        //rand bit rrst_n;
-        rand bit [DSIZE - 1 : 0] wdata;
-        //Output
-        bit [DSIZE - 1 : 0] rdata;
-        bit wfull;
-        bit rempty;
+class wr_seq_item extends uvm_sequence_item;
+  //Inputs
+  rand logic winc;
+  rand logic [DSIZE - 1 : 0] wdata;
+  //Output
+  logic wfull;
+  `uvm_object_utils_begin(wr_seq_item)
+  `uvm_field_int(wdata, UVM_ALL_ON)
+  `uvm_field_int(winc, UVM_ALL_ON)
+  `uvm_field_int(wfull, UVM_ALL_ON)
+  `uvm_object_utils_end
 
-        `uvm_object_utils_begin(seq_item)
-        `uvm_field_int(winc, UVM_DEFAULT);
-        `uvm_field_int(rinc, UVM_DEFAULT);
-        //`uvm_field_int(wrst_n, UVM_DEFAULT);
-        //`uvm_field_int(rrst_n, UVM_DEFAULT);
-        `uvm_field_int(wdata, UVM_DEFAULT);
-        `uvm_field_int(rdata, UVM_DEFAULT);
-        `uvm_field_int(wfull, UVM_DEFAULT);
-        `uvm_field_int(rempty, UVM_DEFAULT);
-        `uvm_object_utils_end
+  function new(string name="wr_sequence_item");
+    super.new(name);
+  endfunction
+endclass
 
-        function new(string name = "seq_item");
-                super.new(name);
-        endfunction
+class rd_seq_item extends uvm_sequence_item;
+  //Inputs
+  rand logic rinc;
+  //Output
+  logic [DSIZE - 1 : 0] rdata;
+  logic rempty;
 
+  `uvm_object_utils_begin(rd_seq_item)
+  `uvm_field_int(rdata, UVM_ALL_ON)
+  `uvm_field_int(rinc, UVM_ALL_ON)
+  `uvm_field_int(rempty, UVM_ALL_ON)
+  `uvm_object_utils_end
+
+  function new(string name="rd_sequence_item");
+    super.new(name);
+  endfunction
 endclass
