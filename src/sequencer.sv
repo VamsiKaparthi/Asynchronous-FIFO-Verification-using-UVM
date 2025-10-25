0fcbbdb670a336533fb3a8d7d7e74280c62cf095
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-10-25 20:27:55 +0530

    Update sequencer.sv

--- src/sequencer.sv
+++ src/sequencer.sv
@@ -1,16 +1,14 @@
-class sequencer extends uvm_sequencer #(seq_item);
-        `uvm_component_utils(sequencer)
-        function new(string name = "sequencer", uvm_component parent = null);
+class rd_sequencer extends uvm_sequencer#(rd_seq_item);
+        `uvm_component_utils(rd_sequencer)
+        function new(string name = "rd_sequencer", uvm_component parent = null);
                 super.new(name, parent);
         endfunction
 endclass
 
-class v_sequencer extends uvm_sequencer #(seq_item);
-        `uvm_component_utils(v_sequencer);
-        sequencer rd_sqr;
-        sequencer wr_sqr;
-
-        function new(string name = "vsqr", uvm_component parent = null);
+class wr_sequencer extends uvm_sequencer#(wr_seq_item);
+        `uvm_component_utils(wr_sequencer)
+        function new(string name = "wr_sequencer", uvm_component parent = null);
                 super.new(name, parent);
         endfunction
 endclass
+
