77ba35921fe28c2ebf487bdbc5bf3bcec1298259
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-10-25 20:30:25 +0530

    Update write_agent.sv

--- src/write_agent.sv
+++ src/write_agent.sv
@@ -1,19 +1,19 @@
 class write_agent extends uvm_agent;
-  `uvm_component_utils(write_agent);
-  sequencer sqr;
-  wr_driver w_drv;
-  wr_monitor w_mon;
-  function new(string name = "write_agent", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    sqr = sequencer::type_id::create("sqr", this);
-    w_drv = wr_driver::type_id::create("w_drv", this);
-    w_mon = wr_monitor::type_id::create("w_mon", this);
-  endfunction
-  function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
-    w_drv.seq_item_port.connect(sqr.seq_item_export);
-  endfunction
+        `uvm_component_utils(write_agent);
+        wr_sequencer sqr;
+        wr_driver w_drv;
+        wr_monitor w_mon;
+        function new(string name = "write_agent", uvm_component parent = null);
+                super.new(name, parent);
+        endfunction
+        function void build_phase(uvm_phase phase);
+                super.build_phase(phase);
+                sqr = wr_sequencer::type_id::create("sqr", this);
+                w_drv = wr_driver::type_id::create("w_drv", this);
+                w_mon = wr_monitor::type_id::create("w_mon", this);
+        endfunction
+        function void connect_phase(uvm_phase phase);
+                super.connect_phase(phase);
+                w_drv.seq_item_port.connect(sqr.seq_item_export);
+        endfunction
 endclass
