cb32d735a14fcdf7aa6e3143d8a1f0468f853336
Kaparthi Vamsi Krishna <vamsikaparthi@feserver.mirafra.com>
2025-11-09 13:34:00 +0530

    Update Tests

--- src/sequence.sv
+++ src/sequence.sv
@@ -111,3 +111,107 @@ class p_write_sequence extends uvm_sequence;
     wait_for_item_done();
         endtask
 endclass
+
+class rempty_write_sequence extends uvm_sequence #(wr_seq_item);
+  `uvm_object_utils(rempty_write_sequence);
+
+  function new(string name="rempty_write_sequence");
+    super.new(name);
+  endfunction
+
+  task body();
+    int count;
+    wr_seq_item item;
+    repeat(4)begin
+      item = wr_seq_item::type_id::create("write_item");
+      wait_for_grant();
+      item.randomize() with {winc == 1;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Write Transaction %0d: winc=%0b, wdata=%0d", $time, count, item.winc, item.wdata), UVM_LOW);
+      send_request(item);
+      wait_for_item_done();
+    end
+    repeat (N) begin
+      item = wr_seq_item::type_id::create("write_item");
+      wait_for_grant();
+      item.randomize() with {winc == 0;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Write Transaction %0d: winc=%0b, wdata=%0d", $time, count, item.winc, item.wdata), UVM_LOW);
+      send_request(item);
+      wait_for_item_done();
+      count++;
+    end
+
+  endtask
+endclass
+
+class rempty_read_sequence extends uvm_sequence #(rd_seq_item);
+  `uvm_object_utils(rempty_read_sequence)
+  function new(string name="rempty_read_sequence");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    int count;
+    rd_seq_item item;
+    repeat(4) begin
+      item = rd_seq_item::type_id::create("read_item");
+      wait_for_grant();
+      item.randomize() with {rinc == 0;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Read Transaction %0d: rinc=%0b", $time, count, item.rinc), UVM_LOW);
+      send_request(item);
+      wait_for_item_done();
+    end
+    repeat(N) begin
+      item = rd_seq_item::type_id::create("read_item");
+      wait_for_grant();
+      item.randomize() with {rinc == 1;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Read Transaction %0d: rinc=%0b", $time, count, item.rinc), UVM_LOW);
+      send_request(item);
+      wait_for_item_done();
+    end
+  endtask
+endclass
+
+class wfull_write_sequence extends uvm_sequence #(wr_seq_item);
+  `uvm_object_utils(wfull_write_sequence);
+
+  function new(string name="wfull_write_sequence");
+    super.new(name);
+  endfunction
+
+  task body();
+    int count;
+    wr_seq_item item;
+    repeat (N) begin
+      item = wr_seq_item::type_id::create("write_item");
+      wait_for_grant();
+      item.randomize() with {winc == 1;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Write Transaction %0d: winc=%0b, wdata=%0d", $time, count, item.winc, item.wdata), UVM_LOW);
+      send_request(item);
+      wait_for_item_done();
+      count++;
+    end
+
+  endtask
+endclass
+
+class wfull_read_sequence extends uvm_sequence #(rd_seq_item);
+  `uvm_object_utils(wfull_read_sequence)
+  function new(string name="rempty_read_sequence");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    int count;
+    rd_seq_item item;
+
+    repeat(N) begin
+      item = rd_seq_item::type_id::create("read_item");
+      wait_for_grant();
+      item.randomize() with {rinc == 0;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Read Transaction %0d: rinc=%0b", $time, count, item.rinc), UVM_LOW);
+      send_request(item);
+      wait_for_item_done();
+    end
+  endtask
+endclass
+
--- src/test.sv
+++ src/test.sv
@@ -53,3 +53,90 @@ class par_wr_test extends uvm_test;
     phase.drop_objection(this);
   endtask
 endclass
+
+class rempty_test extends uvm_test;
+  `uvm_component_utils(rempty_test)
+  env e1;
+  rempty_write_sequence r_write_seq;
+  rempty_read_sequence r_read_seq;
+
+  function new(string name = "rempty_test",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    e1 = env::type_id::create("env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    fork
+      r_write_seq.start(e1.w_agnt.sqr);
+      r_read_seq.start(e1.r_agnt.sqr);
+    join
+    phase.drop_objection(this);
+  endtask
+endclass
+
+class wfull_test extends uvm_test;
+  `uvm_component_utils(wfull_test)
+  env e1;
+  wfull_write_sequence w_write_seq;
+  wfull_read_sequence w_read_seq;
+
+  function new(string name = "wfull_test",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    e1 = env::type_id::create("env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    w_write_seq = wfull_write_sequence::type_id::create("w_write_seq");
+    w_read_seq = wfull_read_sequence::type_id::create("w_read_seq");
+    fork
+      w_write_seq.start(e1.w_agnt.sqr);
+      w_read_seq.start(e1.r_agnt.sqr);
+    join
+    phase.drop_objection(this);
+  endtask
+endclass
+
+class reg_test extends uvm_test;
+  `uvm_component_utils(reg_test)
+  env e1;
+  p_write_sequence p_write_seq;
+  p_read_sequence p_read_seq;
+  write_sequence write_seq;
+  read_sequence read_seq;
+  function new(string name = "reg_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    e1 = env::type_id::create("e1", this);
+    p_write_seq = p_write_sequence::type_id::create("p_write_seq");
+    p_read_seq = p_read_sequence::type_id::create("p_read_seq");
+    write_seq = write_sequence::type_id::create("write_seq");
+    read_seq = read_sequence::type_id::create("read_seq");
+  endfunction
+  task run_phase(uvm_phase phase);
+		phase.raise_objection(this, "Raised");
+    begin
+      fork
+        write_seq.start(e1.w_agnt.sqr);
+        read_seq.start(e1.r_agnt.sqr);
+      join
+      fork
+        p_write_seq.start(e1.w_agnt.sqr);
+        p_read_seq.start(e1.r_agnt.sqr);
+      join
+    end
+		phase.drop_objection(this, "Raised");
+  endtask
+endclass
+
